049c0e38ec7a2b56e7e4dff473709588305ae25b
ljh <2439827731@qq.com>
2025-01-13 21:17:41 +0800

    style(): 下载页样式修改

--- docs/pages/DownloadPage.vue
+++ docs/pages/DownloadPage.vue
@@ -20,9 +20,11 @@ export default {
     const params = useData();
     return {
       params,
+      isDark: params.isDark,
       images: {},
       showPlatform: "Windows",
       logs: [],
+      logsLoading: true,
       downloads: {},
       platforms: [],
       downloadElements: {
@@ -46,57 +48,82 @@ export default {
     const filePrefix = process.env.NODE_ENV === 'production' ? '' : 'test-'
     fetch(`${filePrefix}version-info.json?t=${new Date().getTime()}`).then(async res => {
       const json = await res.json()
-      this.logs = json.logs.filter(v => v.platform === this.showPlatform || v.platform.toLowerCase() === 'all')
+      this.logs = json.logs.filter(v => v.platform === this.showPlatform || v.platform.toLowerCase() === 'all').slice(0, 5)
       this.downloads = json.downloads
       this.images = json.images
+    }).finally(() => {
+      // this.logsLoading = false
+      setTimeout(() => this.logsLoading = false, 300)
     })
   },
   watch: {
     "$route"(newV, oldV) {
       console.log(newV)
+    },
+    "params.isDark"(newV, oldV) {
+      this.isDark = newV
     }
   }
 }
 </script>
 
 <template>
-  <div class="p-6">
+  <div class="p-6" :class="isDark?'download-page-bg-dark':''">
     <div class="text-4xl font-bold text-center text-[transparent] hero-text">
       ClipShare
     </div>
     <div class="text-xl my-6 text-center tracking-[3px]">
       跨平台剪贴板历史记录与同步
     </div>
-    <div class="max-w-[400px] mx-auto" v-show="logs.length">
-      <div class="font-bold text-xl my-4">更新日志</div>
-      <div class="flex" v-for="item in logs">
-        <div class="flex-1">{{ item['desc'] }}</div>
-        <div>{{ item['date'] }}</div>
-      </div>
-      <div class="mt-6 cursor-pointer hover:text-[var(--vp-button-alt-bg)] transition-all duration-200 ease-linear">
-        <a href="/history_version.html" target="_blank">更多日志 ></a>
-      </div>
-    </div>
-    <div class="flex justify-center gap-8 mt-10 flex-wrap">
-      <template v-for="(iconComponent,key) in downloadElements">
-        <div class="platform" v-if="downloads[key]?.url" @click="()=>switchShowPlatform(key)"
-             :style="key===showPlatform?'background: var(--vp-button-brand-bg)':''">
-          <div>
-            <component :is="iconComponent" class="text-[3rem]"/>
-          </div>
-          <div :class="key!==showPlatform?'version':''">{{ downloads[key]?.version }}</div>
+    <transition name="fade" mode="out-in">
+      <div v-if="logsLoading" class="max-w-[400px] mx-auto">
+        <div class="skeleton h-[30px] w-[90px]"/>
+        <div class="flex flex-col py-[15px]">
+          <div class="skeleton skeleton-text" v-for="i in 5"/>
         </div>
-      </template>
-      <div class="platform">
-        <a href="https://github.com/aa2013/ClipShare/releases" target="_blank">
-          <dots-horizontal class="text-[3rem]"/>
-        </a>
-        <div class="version">more</div>
+        <div class="skeleton skeleton-text w-[80px] h-[25px]"/>
+        <div class="flex justify-center gap-8 mt-10 flex-wrap">
+          <div v-for="i in 3" class="skeleton skeleton-image w-[96px] h-[96px]"/>
+        </div>
+        <div class="skeleton rounded-[1.25rem] h-[50px] w-[290px] my-[20px] mx-[auto]"/>
       </div>
-    </div>
-    <a class="download-btn block" :href="downloads[showPlatform]?.url" target="_blank">
-      立即下载（{{ showPlatform }}）{{ downloads[showPlatform]?.version }}
-    </a>
+      <div v-else>
+        <div class="max-w-[400px] mx-auto" v-show="logs.length">
+          <div class="font-bold text-xl my-4">
+            更新日志
+          </div>
+          <div class="max-h-[200px] overflow-auto">
+            <div class="flex mb-[5px]" v-for="item in logs">
+              <pre class="flex-1 whitespace-pre-wrap break-words">{{ item['desc'] }}</pre>
+              <div>{{ item['date'] }}</div>
+            </div>
+          </div>
+          <div class="mt-6 cursor-pointer hover:text-[var(--vp-button-alt-bg)] transition-all duration-200 ease-linear">
+            <a href="/history_version.html" target="_blank">更多日志 ></a>
+          </div>
+        </div>
+        <div class="flex justify-center gap-8 mt-10 flex-wrap">
+          <template v-for="(iconComponent,key) in downloadElements">
+            <div class="platform" v-if="downloads[key]?.url" @click="()=>switchShowPlatform(key)"
+                 :style="key===showPlatform?'background: var(--vp-button-brand-bg)':''">
+              <div>
+                <component :is="iconComponent" class="text-[3rem]"/>
+              </div>
+              <div :class="key!==showPlatform?'version':''">{{ downloads[key]?.version }}</div>
+            </div>
+          </template>
+          <div class="platform">
+            <a href="https://github.com/aa2013/ClipShare/releases" target="_blank">
+              <dots-horizontal class="text-[3rem]"/>
+            </a>
+            <div class="version">more</div>
+          </div>
+        </div>
+        <a class="download-btn block" :href="downloads[showPlatform]?.url" target="_blank">
+          立即下载（{{ showPlatform }}）{{ downloads[showPlatform]?.version }}
+        </a>
+      </div>
+    </transition>
     <div class="stack-container">
       <div v-for="platform in Object.keys(images)" class="stack-item">
         <transition name="fade">
@@ -104,7 +131,7 @@ export default {
                :key="platform">
             <img v-for="item in images[platform]"
                  :src="item.url" :alt="item.name" :width="item.width??300"
-                 class="rounded-[8px] cursor-pointer hover:scale-[1.1] duration-200"
+                 class="rounded-[20px] cursor-pointer hover:scale-[1.05] duration-200"
                  :data-fancybox="`${showPlatform}-img`" loading="eager"/>
           </div>
         </transition>
@@ -114,6 +141,14 @@ export default {
 </template>
 
 <style scoped>
+.download-page-bg-dark {
+  background: linear-gradient(125deg, #1b1b1f, #252b4d, #1b1b1f);
+}
+
+.download-page-bg-light {
+  background: linear-gradient(125deg, #e6f0ff, #fdcbf1 10%, #c2e9fb, #a8edea);
+}
+
 .hero-text {
   background: var(--vp-home-hero-name-background);
   -webkit-text-fill-color: var(--vp-home-hero-name-background);
@@ -180,13 +215,5 @@ export default {
 .stack-item {
   grid-column: 1 / 2; /* 所有子元素放在同一列 */
   grid-row: 1 / 2; /* 所有子元素放在同一行 */
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 1s;
-}
-
-.fade-enter-from, .fade-leave-to {
-  opacity: 0;
 }
 </style>
